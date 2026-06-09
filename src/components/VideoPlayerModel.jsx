2d7843980aaf3b8a7fd0150420928fb087b15964
Controluser <changeuser132@gmail.com>
2026-06-09 22:55:29 +0530

    play trailer

--- src/components/VideoPlayerModel.jsx
+++ src/components/VideoPlayerModel.jsx
@@ -13,7 +13,7 @@ const VideoPlayerModel = ({ setIsVideoOpen, trailerKey }) => {
 
   return (
     <div className="absolute bg-black inset-0 z-50 flex items-center justify-center">
-      <button onClick={closePlayer} className="absolute top-10 left-10 bg-gray-500/50 backdrop-blur-sm p-3 rounded-full hover:cursor-pointer">
+      <button onClick={closePlayer} className="absolute top-10 left-5 bg-gray-500/50 backdrop-blur-sm p-3 rounded-full hover:cursor-pointer">
         <FaArrowLeft size={18} className="text-slate-100" />
       </button>
       <div className="yt-emmed-holder rounded-md w-full overflow-hidden aspect-video">
